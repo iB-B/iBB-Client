7ba848de334dc874ea44dec1f7848ca982770f10
Peridax <evandrojr902@gmail.com>
2021-03-24 13:14:11 -0400

    Tweaked nav, tweaked homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { withRouter } from 'react-router-dom'
 
 import Jumbotron from 'react-bootstrap/Jumbotron'
@@ -11,12 +11,18 @@ class Home extends Component {
 
   render () {
     return (
-      <Jumbotron className="hero-banner" fluid>
-        <div className="form-floating mb-3">
-          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-          <label htmlFor="floatingInput">Where are you going?</label>
+      <Fragment>
+        <Jumbotron className="hero-banner" fluid>
+          <div className="form-floating mb-3">
+            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+            <label htmlFor="floatingInput">Where are you going?</label>
+          </div>
+        </Jumbotron>
+
+        <div className="container-section">
+          <h6 className="section-title">Locals Favorite Gems</h6>
         </div>
-      </Jumbotron>
+      </Fragment>
     )
   }
 }
