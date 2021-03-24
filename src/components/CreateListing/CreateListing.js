833fccdd21ebd584b3e895824e918dc68219a8d5
zanhorst <zanderhorstman@gmail.com>
2021-03-24 16:37:20 -0400

    added listing.js to src/api. potentially finished CreateListing

--- src/components/CreateListing/CreateListing.js
+++ src/components/CreateListing/CreateListing.js
@@ -14,8 +14,13 @@ class CreateListing extends Component {
     super(props)
 
     this.state = {
-      location: '',
-      dateAvail: '',
+      name: '',
+      address: '',
+      city: '',
+      stateProvince: '',
+      postalCode: '',
+      latitude: '',
+      longitude: '',
       price: '',
       description: ''
     }
@@ -38,7 +43,7 @@ class CreateListing extends Component {
         }))
         .then(() => history.push('/index-my-listings'))
         .catch(error => {
-          this.setState({ title: '', body: '' })
+          this.setState({ name: '', address: '', city: '', stateProvince: '', postalCode: '', latitude: '', longitude: '', price: '', description: '' })
           msgAlert({
             heading: 'Listing Failed with error: ' + error.message,
             message: messages.createListingFailure,
@@ -49,53 +54,108 @@ class CreateListing extends Component {
   }
 
   render () {
-    const { title, body } = this.state
+    const { name, address, city, stateProvince, postalCode, latitude, longitude, price, description } = this.state
 
     return (
       <div className="row">
         <Row>
           <Form onSubmit={this.onCreateListing}>
-            <Form.Group controlId="title">
+            <Form.Group controlId="name">
               <Form.Label>Name</Form.Label>
               <Form.Control
                 required
                 type="text"
                 name="name"
                 value={name}
-                placeholder="Enter a title"
+                placeholder="Enter a name"
                 onChange={this.handleChange}
               />
             </Form.Group>
-            <Form.Group controlId="body">
-              <Form.Label>Location</Form.Label>
+            <Form.Group controlId="address">
+              <Form.Label>Address</Form.Label>
               <Form.Control
                 required
                 type="text"
-                name="location"
-                value={location}
-                placeholder="Type something!"
+                name="address"
+                value={address}
+                placeholder="Enter the address of your lodge."
                 onChange={this.handleChange}
               />
             </Form.Group>
-            <Form.Group controlId="body">
-              <Form.Label>Available Dates</Form.Label>
+            <Form.Group controlId="city">
+              <Form.Label>City</Form.Label>
               <Form.Control
                 required
                 type="text"
-                name="date"
-                value={dateAvail}
-                placeholder="Type something!"
+                name="city"
+                value={city}
+                placeholder="Enter your city."
                 onChange={this.handleChange}
               />
             </Form.Group>
-            <Form.Group controlId="body">
+            <Form.Group controlId="stateProvince">
+              <Form.Label>State / Province</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                name="stateProvince"
+                value={stateProvince}
+                placeholder="Enter your state or province."
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="postalCode">
+              <Form.Label>Postal code</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                name="postalCode"
+                value={postalCode}
+                placeholder="Enter your postal code."
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="latitude">
+              <Form.Label>Latitude</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                name="latitude"
+                value={latitude}
+                placeholder="Enter the latitude"
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="longitude">
+              <Form.Label>Longitude</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                name="longitude"
+                value={longitude}
+                placeholder="Enter the longitude"
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="price">
               <Form.Label>Price</Form.Label>
               <Form.Control
                 required
                 type="text"
                 name="price"
                 value={price}
-                placeholder="Type something!"
+                placeholder="Enter your asking price."
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="description">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                name="description"
+                value={description}
+                placeholder="Enter a description of your lodge."
                 onChange={this.handleChange}
               />
             </Form.Group>
